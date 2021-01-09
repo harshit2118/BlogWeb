6dc54bb8a6f395cc35d113093b55985804dafdb1
Harshit Joshi <hrshtjoshi238@gmail.com>
2021-01-09 10:32:42 +0530

    some style and layout changes

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(express.static("local"));
 app.set("view engine","ejs");
 
 const home_text    = "Welcome to my DD. If you want to share anything about your self and also want to share your deepest secrets of your life then you can easily share it with DD. DD is your best friend as DD didn't judge you for anything that you have done.";
-const about_text   =  "DD is your personal online diary or blog where you can write about your self daily. DD is a standalon project which is developed by <b>Harshit Joshi</b> as it web development projects. ";
 const contact_text_emial = "hrshtjoshi238@gmail.com";
 const contact_text_linkedin = "https://linkedin.com/in/harshit-joshi-5a5782149";
 const contact_text_github = "https://github.com/harshit2118";
@@ -57,11 +56,11 @@ app.get("/contact",(req,res)=>{
         ghub : contact_text_github 
     });
 });
+
 app.get("/about",(req,res)=>{
-    res.render("about",{
-        aboutText : about_text
-    });
+    res.render("about");
 });
+
 app.get("/compose",(req,res)=>{
     res.render("compose");
 });
